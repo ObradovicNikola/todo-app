fd08d9fb91f369ca0be8eeaded419e0637e813e4
Obradovic Nikola <obradovicnikola009@gmail.com>
2019-08-29 10:31:04 +0200

    Initial build commit

---
+++ precache-manifest.42771b6885d156310321e59ea7a11aab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3593b4817e9fd519114ab3fba4810728",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "8e0cb93cc5adb6a749d0",
+    "url": "/todo-app/static/css/main.7f5fc7b8.chunk.css"
+  },
+  {
+    "revision": "752945d3d186a4424e6f",
+    "url": "/todo-app/static/js/2.5ba774ce.chunk.js"
+  },
+  {
+    "revision": "8e0cb93cc5adb6a749d0",
+    "url": "/todo-app/static/js/main.97d791cc.chunk.js"
+  },
+  {
+    "revision": "22a810c513893e7bf031",
+    "url": "/todo-app/static/js/runtime~main.5cdee4a4.js"
+  }
+]);
